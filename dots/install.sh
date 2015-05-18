5466cb63911b953d3f2858c18e9632a85d90d55c
Clann <clann24@gmail.com>
2015-05-18 11:21:10 +0800

    i

--- dots/install.sh
+++ dots/install.sh
@@ -1,2 +1,19 @@
-cp bash_profile ~/.bash_profile
-source ~/.bash_profile
+
+
+
+if [ "$(uname)" == "Darwin" ]; then
+  # Enable tab completion
+  brew install git bash-completion
+
+  cp bash_profile ~/.bash_profile
+  printf "\nif [ -f `brew --prefix`/etc/bash_completion ]; then\n  . `brew --prefix`/etc/bash_completion;\nfi\n" >> ~/xx
+  source ~/.bash_profile
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  # Enable tab completion
+  sudo apt-get install git bash-completion
+  
+  cp bash_profile ~/.bashrc
+  source ~/.bashrc
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    # Do something under Windows NT platform
+fi
