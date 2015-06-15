0d2257884e2b05f8a9e7580ce9368f3d6447f3d7
Shian Chen <clann24@gmail.com>
2015-06-15 12:51:39 +0800

    i

--- dots/install.sh
+++ dots/install.sh
@@ -11,9 +11,9 @@ if [ "$(uname)" == "Darwin" ]; then
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   # Enable tab completion
   sudo apt-get install git bash-completion
-  
-  cp bash_profile ~/.bash_profile
-  source ~/.bash_profile
+  mv ~/.bashrc ~/.bashrc_backup  
+  cp bash_profile ~/.bashrc
+  source ~/.bashrc
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
   # Do something under Windows NT platform
   echo "nothing"
