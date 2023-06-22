89df4dd2f78997890d24f550906ea25466c5bbc9
Shian Chen <Clann24@gmail.com>
2023-06-22 10:44:03 +0800

    Update install.sh

--- dots/install.sh
+++ dots/install.sh
@@ -1,20 +1,12 @@
-
-
-
-if [ "$(uname)" == "Darwin" ]; then
-  # Enable tab completion
-  brew install git bash-completion
-
-  cp bash_profile ~/.bash_profile
-  printf "\nif [ -f `brew --prefix`/etc/bash_completion ]; then\n  . `brew --prefix`/etc/bash_completion;\nfi\n" >> ~/xx
-  source ~/.bash_profile
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  # Enable tab completion
-  sudo apt-get install git bash-completion
-  mv ~/.bashrc ~/.bashrc_backup  
-  cp bash_profile ~/.bashrc
-  source ~/.bashrc
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-  # Do something under Windows NT platform
-  echo "nothing"
-fi
+# brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/[username]/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+brew install --cask iterm2
+brew install git
+# Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Power10k
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+echo ZSH_THEME=\"powerlevel10k/powerlevel10k\" >>~/.zshrc
+source ~/.zshrc
